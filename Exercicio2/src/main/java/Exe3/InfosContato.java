1f8b21e18ee1fb4263382f86df95ef963ded3e94
MazinhaB <mariana.barude@gec.inatel.br>
2022-08-16 18:16:10 -0300

    InfosContato

---
+++ Exercicio2/src/main/java/Exe3/InfosContato.java
@@ -0,0 +1,14 @@
+package Exe3;
+
+public class InfosContato {
+    String email;
+    int numTelefone;
+    String enderecoCasa;
+
+    public void mostraInfo(){
+        System.out.println("Email: "+this.email);
+        System.out.println("Numero de telefone: "+this.numTelefone);
+        System.out.println("Endereco: "+this.enderecoCasa);
+    }
+
+}
